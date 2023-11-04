d714a1b3114c744002beec8df5f58edb113d8ecd
Migram05 <100275263+Migram05@users.noreply.github.com>
2023-11-04 15:55:46 +0100

    Cambios de calidad de vida
    Se han añadido algunos cambios de calidad de vida: en la escena de escribir textos, se ha añadido un máximo número de líneas para evitar que cuando se muestre el texto, la letra sea mu pequeña. Y ahora todos los botones empiezan desactivados y se vuelven a activar a los 0.5 segundos de ser creados, para evitar hacer clicks sin querer

--- APP_TFG/Assets/Scripts/DailyScenes/ButtonScript.cs
+++ APP_TFG/Assets/Scripts/DailyScenes/ButtonScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
@@ -11,5 +13,17 @@ public class ButtonScript : MonoBehaviour
     public void onClicked()
     {
         StartCoroutine(Manager.instance.LoadSceneDelayed(delayTime, sceneNum));
-    } 
+    }
+    private void Start()
+    {
+        gameObject.GetComponent<Button>().interactable = false; //Para evitar un click sin querer
+        StartCoroutine(enableButton());
+    }
+
+    public IEnumerator enableButton() 
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameObject.GetComponent<Button>().interactable = true;
+    }
+
 }
